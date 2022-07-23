cf68a4b34138b709294c2192858738561f03aa6e
noah <isnoah2022@hotmail.com>
2022-07-22 22:28:13 -0400

    NoahV2

--- src/components/mySkillsComponents.jsx
+++ src/components/mySkillsComponents.jsx
@@ -7,29 +7,33 @@ import {
   faNodeJs,
   faReact,
 } from "@fortawesome/free-brands-svg-icons";
+import Slide from "react-reveal/Slide";
 
 export const _mySkillsComponents = () => {
   return (
-    <div className="App-Skills">
-      <h2>Skills:</h2>
-      <div className="iAm">
-        <p>
-          Hello My name is Andriel Noa Lores and I am a web developer, we build
-          web applications with quality and unique styles.
-        </p>
+    <Slide bottom>
+      <div className="App-Skills">
+        <h2>Skills:</h2>
+        <div className="iAm">
+          <p>
+            Hello My name is Andriel Noa Lores and I am a web developer, we
+            build web applications with quality and unique styles.
+          </p>
+        </div>
+
+        <div className="skillsByNoah">
+
+          <FontAwesomeIcon icon={faReact} />
+
+          <FontAwesomeIcon icon={faNodeJs} />
+
+          <FontAwesomeIcon icon={faJs} />
+
+          <FontAwesomeIcon icon={faHtml5} />
+
+          <FontAwesomeIcon icon={faCss3} />
+        </div>
       </div>
-
-      <div className="skillsByNoah">
-        <FontAwesomeIcon icon={faReact} />
-
-        <FontAwesomeIcon icon={faNodeJs} />
-
-        <FontAwesomeIcon icon={faJs} />
-
-        <FontAwesomeIcon icon={faHtml5} />
-
-        <FontAwesomeIcon icon={faCss3} />
-      </div>
-    </div>
+    </Slide>
   );
 };
